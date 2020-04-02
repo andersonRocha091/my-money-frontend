81c6e1ffc7c77df20ce63b841acf17aaf34e0945
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-01 22:52:27 -0300

    Incluindo componente row para alinhamento dos componentes na tela

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -3,19 +3,22 @@ import React, { Component } from 'react';
 import ContentHeader from '../common/template/ContentHeader';
 import Content from '../common/template/Content';
 import ValueBox from '../common/widget/ValueBox';
+import Row from '../common/layout/Row';
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <ContentHeader title='Dashboard' small='Versão 1.0'/>
+                <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    <ValueBox cols='12 6 3' color='green' icon='ion-social-usd-outline'
-                    value="10,00" text="Total de Créditos"/>
-                    <ValueBox cols='12 6 3' color='red' icon='ion-ios-pricetag-outline'
-                    value="10,00" text="Total de Débitos"/>
-                    <ValueBox cols='12 6 3' color='blue' icon='ion-ios-pie-outline'
-                    value="10,00" text="Valor Consolidado"/>
+                    <Row>
+                        <ValueBox cols='12 6 3' color='green' icon='ion-social-usd-outline'
+                            value="10,00" text="Total de Créditos" />
+                        <ValueBox cols='12 6 3' color='red' icon='ion-ios-pricetag-outline'
+                            value="10,00" text="Total de Débitos" />
+                        <ValueBox cols='12 6 3' color='blue' icon='ion-ios-pie-outline'
+                            value="10,00" text="Valor Consolidado" />
+                    </Row>
                 </Content>
             </div>
         )
