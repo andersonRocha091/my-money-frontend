db69dc14f82895db1dc638b2dfd217fb6a810e7b
Anderson Rocha <andersonecomp091@gmail.com>
2020-04-12 10:39:00 -0300

    Finalizada a inclusao do componente toastr para exibicao de erros e confirmação

--- src/billingcycle/BillingCycleActions.js
+++ src/billingcycle/BillingCycleActions.js
@@ -1,4 +1,5 @@
 import Axios from 'axios';
+import { toastr } from 'react-redux-toastr'
 const BASE_URL = 'http://localhost:3003/api';
 
 export function getList() {
@@ -10,8 +11,14 @@ export function getList() {
 }
 
 export function create(values) {
-    Axios.post(`${BASE_URL}/billingCycles`,values)
-    return{
-        type:'TEMP'
+    Axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação realizada com sucesso.')
+        })
+        .catch(e => {
+            e.response.data.errors.forEach(error => toastr.error('Erro', error));
+        })
+    return {
+        type: 'TEMP'
     }
 }
--- src/main/App.js
+++ src/main/App.js
@@ -6,6 +6,7 @@ import Header from '../common/template/Header';
 import Sidebar from '../common/template/Sidebar';
 import Footer from '../common/template/Footer';
 import Routes from './Routes';
+import Messages from '../common/msg/Messages';
 
 export default props => (
   <div>
@@ -15,35 +16,6 @@ export default props => (
       <Routes/>
     </div>
     <Footer />
+    <Messages/>
   </div>
 )
-
-
-
-
-// import React from 'react';
-// import logo from '../logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
